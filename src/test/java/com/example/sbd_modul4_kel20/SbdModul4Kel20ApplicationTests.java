66e1e65fe3ea28f24004b6a6b30706aeeaad2640
unknown <hxfxzh001@gmail.com>
2023-10-22 12:34:04 +0700

    first commit

---
+++ src/test/java/com/example/sbd_modul4_kel20/SbdModul4Kel20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbd_modul4_kel20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdModul4Kel20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
